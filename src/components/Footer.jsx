3f09a86e41edab2aed1226fcf4765fd7ba04a86e
riritriana <riritriana21@gmail.com>
2025-08-11 11:03:21 +0700

    update

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+export default function Footer() {
+  return (
+    <footer className="footer-container">
+      <div className="footer-content">
+        <p>© 2025 Riri Triana. All Rights Reserved.</p>
+        <p>
+          Contact me via <a href="mailto:riritriana21@gmail.com">email</a> or
+          phone.
+        </p>
+        <span className="phone">📞 +62 572 3764 893</span>
+      </div>
+    </footer>
+  );
+}
